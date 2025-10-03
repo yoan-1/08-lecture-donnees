454a2fa6aa44ffe22724beaabb42513498898766
yoan-oztopal <yoan.oztopal@edu.esiee.fr>
2025-10-03 10:47:48 +0000

    6.67, je ne suis pas allé plus haut. j'ai utilisé le cours, j'ai trouvé .strip avec copilot pour améliorer le code

--- main.py
+++ main.py
@@ -1,52 +1,21 @@
-#### Imports et définition des variables globales
+
+"""
+Module de lecture de fichier texte ligne par ligne.
+Ce module permet de lire un fichier texte et de retourner son contenu sous forme de liste de lignes.
+"""
 
 FILENAME = "listes.csv"
 
-#### Fonctions secondaires
-
 def read_data(filename):
-    """retourne le contenu du fichier <filename>
+    list_lines = []
+    with open(filename, mode='r', encoding='utf8') as f:
+        for elt in f:  
+            elt = elt.strip()
+            list_lines.append(elt)
+            print(elt)
+    return list_lines
+    return list_lines
 
-    Args:
-        filename (str): nom du fichier à lire
+data = read_data(FILENAME)
+print(data)
 
-    Returns:
-        list: le contenu du fichier (1 list par ligne)
-    """
-    l = []
-    return l
-
-def get_list_k(data, k):
-    l = []
-    return l
-
-def get_first(l):
-    return None
-
-def get_last(l):
-    return None
-
-def get_max(l):
-    return None
-
-def get_min(l):
-    return None
-
-def get_sum(l):
-    return None
-
-
-#### Fonction principale
-
-
-def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
-
-
-if __name__ == "__main__":
-    main()
